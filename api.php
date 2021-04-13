4232e7ad63d42faa15df24551429f0099ab11283
KarlAldousBanaag <gangzarrai@gmail.com>
2021-04-13 08:53:43 +0800

    PHP code

---
+++ api.php
@@ -0,0 +1,23 @@
+<?php
+// Optional Name
+// https://stackoverflow.com/questions/4064444/returning-json-from-a-php-script
+
+header('Content-type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $me = [ 'order_ID' => 301684,
+            'first_name' => 'Juan',
+            'last_name' => 'dela Cruz',
+            'order' => [
+                'dalgona' => 1,
+                'fries' => 1
+            ],
+            'address' => 'Jesus St, Pandacan, Manila, 1011 Metro Manila'
+        ];
+    echo json_encode($me);
+} else {
+    http_response_code(405);
+    echo '{"Error": "Method not supported."}';
+}
+
+?>
